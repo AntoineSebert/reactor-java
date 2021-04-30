53f58326241a09cdee4ae481fb2ab02191526481
Antoine <antoine.sb@orange.fr>
2021-04-30 20:59:18 +0200

    add test

---
+++ src/test/java/TestReactor.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import reactor.Reactor;
+import reactor.Reaction;
+import reactor.Trigger;
+import target.Target;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class TestReactor {
+	@Test
+	public void testHelloWorld() {
+		HashSet<Target> targets = new HashSet<>(1);
+		targets.add(Target.Java);
+
+		HashSet<Trigger> triggers = new HashSet<>(1);
+		triggers.add(Trigger.STARTUP);
+
+		Function<Reaction, Void> hello_world = (reaction) -> {
+			System.out.println("Hello World.\n");
+			return null;
+		};
+
+		HashSet<Reaction> reactions = new HashSet<>(1);
+		reactions.add(new Reaction(triggers, new HashSet<>(0), new HashSet<>(0), (reaction) -> {
+			System.out.println("Hello World.\n");
+			return null;
+		}));
+
+		Reactor main = new Reactor(
+				"Minimal",
+				new HashSet<>(0),
+				new HashSet<>(0),
+				new HashSet<>(0),
+				new HashSet<>(0),
+				new HashSet<>(0),
+				new HashSet<>(0),
+				reactions,
+				new HashSet<>(0),
+				"");
+
+		new Program(targets, new HashSet<>(0), new HashSet<>(0), Optional.of(main)).run();
+	}
+}
