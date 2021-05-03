579d19da09092df9e6f5b3ffdaf01a2962720f31
Antoine <antoine.sb@orange.fr>
2021-05-03 13:30:31 +0200

    update test

--- src/test/java/TestReactor.java
+++ src/test/java/TestReactor.java
@@ -1,40 +1,32 @@
 import org.junit.jupiter.api.Test;
-import _import.Import;
-import reactor.Reactor;
 import reactor.Reaction;
+import reactor.Reactor;
 import reactor.Trigger;
 import target.Target;
+
 import java.util.HashSet;
-import java.util.Optional;
-import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestReactor {
 	@Test
 	public void testHelloWorld() {
-		HashSet<Target> targets = new HashSet<>(1);
-		targets.add(Target.Java);
-
-		HashSet<Trigger> triggers = new HashSet<>(1);
-		triggers.add(Trigger.STARTUP);
-
-		HashSet<Reaction> reactions = new HashSet<>(1);
-		reactions.add(new Reaction(triggers, new HashSet<>(0), new HashSet<>(0), (reaction) -> {
-			System.out.println("Hello World.\n");
-			return null;
-		}));
-
-		Reactor main = new Reactor(
-				"Minimal",
-				new HashSet<>(0),
-				new HashSet<>(0),
-				new HashSet<>(0),
-				new HashSet<>(0),
-				new HashSet<>(0),
-				new HashSet<>(0),
-				reactions,
-				new HashSet<>(0),
-				"");
-
-		new Program(targets, new HashSet<>(0), new HashSet<>(0), Optional.of(main)).run();
+		(new Program.Builder())
+				.targets(Stream.of(Target.Java).collect(Collectors.toCollection(HashSet::new)))
+				.mainReactor((new Reactor.Builder("Minimal"))
+						.reactions(Stream.of(
+								(new Reaction.Builder())
+										.targetCode((reaction) -> {
+											System.out.println("Hello World.\n");
+											return null;
+										})
+										.triggers(Stream.of(Trigger.STARTUP).collect(
+												Collectors.toCollection(HashSet::new)
+										))
+										.build()
+						).collect(Collectors.toCollection(HashSet::new)))
+						.build())
+				.build()
+				.run();
 	}
 }
