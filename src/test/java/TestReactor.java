14e9d531d013f9d99bcd2ac4e120ed3d6f903825
Antoine <antoine.sb@orange.fr>
2021-05-06 11:43:43 +0200

    update tests

--- src/test/java/TestReactor.java
+++ src/test/java/TestReactor.java
@@ -1,32 +1,29 @@
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 import reactor.Reaction;
-import reactor.Reactor;
+import reactor.ReactorVar;
 import reactor.Trigger;
 import target.Target;
-
-import java.util.HashSet;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TestReactor {
 	@Test
 	public void testHelloWorld() {
-		(new Program.Builder())
-				.targets(Stream.of(Target.Java).collect(Collectors.toCollection(HashSet::new)))
-				.mainReactor((new Reactor.Builder("Minimal"))
-						.reactions(Stream.of(
-								(new Reaction.Builder())
-										.targetCode((reaction) -> {
-											System.out.println("Hello World.\n");
-											return null;
-										})
-										.triggers(Stream.of(Trigger.STARTUP).collect(
-												Collectors.toCollection(HashSet::new)
-										))
-										.build()
-						).collect(Collectors.toCollection(HashSet::new)))
-						.build())
-				.build()
-				.run();
+		assertDoesNotThrow(
+				() -> (new Program.Builder())
+					.addTarget(Target.Java)
+					.mainReactor((new ReactorVar.Builder("Minimal"))
+							.addReaction((new Reaction.Builder())
+									.targetCode((reaction) -> {
+										System.out.println("Hello World.\n");
+
+										return null;
+									})
+									.addTrigger(new Trigger.STARTUP())
+									.build()
+							).build())
+					.build()
+					.run()
+		);
 	}
 }
