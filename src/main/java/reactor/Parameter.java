ddeac13f70a7d7de8a0e4d30676aad13460a94b9
Antoine <antoine.sb@orange.fr>
2021-05-06 10:05:53 +0200

    remove Declaration extension

--- src/main/java/reactor/Parameter.java
+++ src/main/java/reactor/Parameter.java
@@ -3,17 +3,38 @@ package reactor;
 /**
  * https://github.com/icyphy/lingua-franca/wiki/Language-Specification#parameter-declaration
  */
-public class Parameter<T> extends Declaration {
+public class Parameter<T> {
 	private String name;
 	private final T value;
 
 	public Parameter(String name, T value) {
-		super(name);
+		if (name.isEmpty())
+			throw new ExceptionInInitializerError(getClass().getTypeName() + " name cannot be empty");
 
+		this.name = name;
 		this.value = value;
 	}
 
 	public T value() {
 		return value;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String name() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		return name.equals(((Parameter<?>) o).name);
 	}
 }
