dc342b13e03e88c7402f71ce8bbc625fa6a1fc4b
Antoine <antoine.sb@orange.fr>
2021-05-08 13:17:09 +0200

    add scheduling structures

--- src/main/java/reactor/Reactor.java
+++ src/main/java/reactor/Reactor.java
@@ -1,6 +1,11 @@
 package reactor;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Reactor specification class.
@@ -8,6 +13,13 @@ import java.io.IOException;
  * https://github.com/icyphy/lingua-franca/wiki/Language-Specification#contained-reactors
  */
 public abstract class Reactor extends Declaration {
+
+	protected Map<Trigger, Reaction> pool = new HashMap<Trigger, Reaction>();
+
+	protected Queue<Reaction> exec_q = new LinkedList<>();
+
+	AtomicBoolean in_exec_q = new AtomicBoolean(false);
+
 	protected Reactor(String name) {
 		super(name);
 	}
