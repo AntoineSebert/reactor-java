8fd54b04878666521cdf2dd372b302177d62abd8
Antoine <antoine.sb@orange.fr>
2021-05-11 22:35:45 +0200

    update import to work with Program

--- src/main/java/_import/Import.java
+++ src/main/java/_import/Import.java
@@ -1,39 +1,31 @@
 package _import;
 
+import org.jetbrains.annotations.NotNull;
+import program.Program;
 import reactor.Reactor;
 
-import java.io.File;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Import {
-	HashMap<String, Reactor> reactors;
+	private HashSet<Reactor> reactors;
 
-	File file;
-
-	File getFile() {
-		return file;
+	public Import(@NotNull Program program) {
+		reactors = program.getReactors();
 	}
 
-	HashMap<String, Reactor> getReactor() {
+	public HashSet<Reactor> getReactors() {
 		return reactors;
-	}
-
-	public Import(File file) {
-		if (!file.exists())
-			throw new ExceptionInInitializerError("The field to import does not exists");
-
-		this.file = file;
 	}
 
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
-		return file.equals(((Import) o).file);
+		return reactors.equals(((Import) o).reactors);
 	}
 
 	@Override
 	public int hashCode() {
-		return file.hashCode();
+		return reactors.hashCode();
 	}
 }
