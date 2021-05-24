ff6f0d7463368db8c1850742767d952562c722bd
Antoine <antoine.sb@orange.fr>
2021-05-24 09:14:21 +0200

    improve connection

--- src/main/java/reactor/Connection.java
+++ src/main/java/reactor/Connection.java
@@ -5,15 +5,68 @@ import reactor.port.Input;
 import reactor.port.Output;
 
 import java.time.Duration;
-import java.util.Optional;
 
-public record Connection<T>(@NotNull Input<T> input, @NotNull Output<T> output, @NotNull Optional<Duration> after,
-                           boolean physical) implements Statement {
+public class Connection<T> implements Statement {
+	private Input<T> input;
+	private Output<T> output;
+	String[] input_name;
+	String[] output_name;
+	private Duration after = Duration.ZERO;
+	private boolean physical;
+
+	public Connection(@NotNull String[] input_name, @NotNull String[] output_name) {
+		if(input_name.length == 0 || output_name.length == 0)
+			throw new ExceptionInInitializerError("Connection must specify input and output");
+
+		this.input_name = input_name;
+		this.output_name = output_name;
+	}
+
+	public boolean isInitialized() {
+		return input != null && output != null;
+	}
+
+	public void output(Output<?> output) {
+		this.output = (Output<T>) output;
+	}
+
+	public void input(Input<?> input) {
+		this.input = (Input<T>) input;
+	}
+
+	public Output<T> output() {
+		return output;
+	}
+
+	public Input<T> input() {
+		return input;
+	}
+
+	public boolean physical() {
+		return physical;
+	}
+
+	public Connection<T> physical(boolean physical) {
+		this.physical = physical;
+
+		return this;
+	}
+
+	public Duration after() {
+		return after;
+	}
+
+	public Connection<T> after(Duration after) {
+		this.after = after;
+
+		return this;
+	}
+
 	public void set(T value) {
 		long msg_time = physical ? Time.physical() : output.timestamp();
 
-		if (after.isPresent())
-			msg_time += after.get().toNanos();
+		if (after != Duration.ZERO)
+			msg_time += after.toNanos();
 
 		input.set(value, msg_time);
 	}
